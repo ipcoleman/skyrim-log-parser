ef4cbabca93f85d85298ab57aabd23c94ac64d25
Ian Coleman <colemip@gmail.com>
2013-02-12 23:16:17 -0500

    [#7] Created PlayerDialogueEvent

---
+++ ths/PlayerDialogueEvent.java
@@ -0,0 +1,50 @@
+package ths;
+
+public class PlayerDialogueEvent extends Event {
+
+	private String actorID;
+	private String actorName;
+	private String topicID;
+	
+	public PlayerDialogueEvent(String line) {
+		super(line);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getActorID() {
+		return actorID;
+	}
+
+	public void setActorID(String actorID) {
+		this.actorID = actorID;
+	}
+
+	public String getActorName() {
+		return actorName;
+	}
+
+	public void setActorName(String actorName) {
+		this.actorName = actorName;
+	}
+
+	public String getTopicID() {
+		return topicID;
+	}
+
+	public void setTopicID(String topicID) {
+		this.topicID = topicID;
+	}
+
+	@Override
+	protected void parse() {
+		// TODO Auto-generated method stub
+		super.parse();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
