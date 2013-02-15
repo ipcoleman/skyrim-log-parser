d2d11eacec89284323a50239e89ff3e95600615a
Ian Coleman <colemip@gmail.com>
2013-02-14 22:25:53 -0500

    [#13] Created QuestCompletedEvent

---
+++ ths/QuestCompletedEvent.java
@@ -0,0 +1,42 @@
+package ths;
+
+public class QuestCompletedEvent extends Event {
+
+	private String questID;
+	private String questName;
+	
+	public QuestCompletedEvent(String line) {
+		super(line);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getQuestID() {
+		return questID;
+	}
+
+	public void setQuestID(String questID) {
+		this.questID = questID;
+	}
+
+	public String getQuestName() {
+		return questName;
+	}
+
+	public void setQuestName(String questName) {
+		this.questName = questName;
+	}
+
+	@Override
+	protected void parse() {
+		// TODO Auto-generated method stub
+		super.parse();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
+
