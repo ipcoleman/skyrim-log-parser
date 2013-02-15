e8c8c85e8325bde5375ec746af163bebb0dc710e
Ian Coleman <colemip@gmail.com>
2013-02-14 22:22:06 -0500

    [#12] Created PlayerBookReadEvent

---
+++ ths/PlayerBookReadEvent.java
@@ -0,0 +1,42 @@
+package ths;
+
+public class PlayerBookReadEvent extends Event {
+
+	private String bookID;
+	private String bookName;
+	
+	public PlayerBookReadEvent(String line) {
+		super(line);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getBookID() {
+		return bookID;
+	}
+
+	public void setBookID(String bookID) {
+		this.bookID = bookID;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	@Override
+	protected void parse() {
+		// TODO Auto-generated method stub
+		super.parse();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
+
