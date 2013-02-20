9ae99b175c49bb671112c6ce91fd189ec980434d
Ian Coleman <colemip@gmail.com>
2013-02-20 00:26:52 -0500

    Removed weaponname and weaponid attributes since they are not included in the log file

--- src/ths/PlayerWeaponWithdrawnEvent.java
+++ src/ths/PlayerWeaponWithdrawnEvent.java
@@ -2,29 +2,28 @@ package src.ths;
 
 public class PlayerWeaponWithdrawnEvent extends Event {
 
-	private String weaponID;
-	private String weaponName;
+//	private String weaponID;
+//	private String weaponName;
 	
 	public PlayerWeaponWithdrawnEvent(String line) {
 		super(line);
-		// TODO Auto-generated constructor stub
 	}
 
-	public String getWeaponID() {
-		return weaponID;
-	}
-
-	public void setWeaponID(String weaponID) {
-		this.weaponID = weaponID;
-	}
-
-	public String getWeaponName() {
-		return weaponName;
-	}
-
-	public void setWeaponName(String weaponName) {
-		this.weaponName = weaponName;
-	}
+//	public String getWeaponID() {
+//		return weaponID;
+//	}
+//
+//	public void setWeaponID(String weaponID) {
+//		this.weaponID = weaponID;
+//	}
+//
+//	public String getWeaponName() {
+//		return weaponName;
+//	}
+//
+//	public void setWeaponName(String weaponName) {
+//		this.weaponName = weaponName;
+//	}
 	
 	@Override
 	protected void parse() {
@@ -42,8 +41,8 @@ public class PlayerWeaponWithdrawnEvent extends Event {
 	{
 		if(this.tag.contains("PLAYER_WEAPON_WITHDRAWN"))
 		{			
-			setWeaponID(parseWeaponID());
-			setWeaponName(parseWeaponName());
+//			setWeaponID(parseWeaponID());
+//			setWeaponName(parseWeaponName());
 		}
 		else
 		{
@@ -51,48 +50,48 @@ public class PlayerWeaponWithdrawnEvent extends Event {
 		}
 	}
 	
-	private String parseWeaponID()
-	{
-		String wHand = "";
-		int startIndex, endIndex, searchIndex;
-		String searchTerm = "[WEAPON";
-		searchIndex = line.indexOf(searchTerm);
-		
-		if(searchIndex >= 0)
-		{
-			startIndex = searchIndex + searchTerm.length() + 4;
-			endIndex = line.indexOf(')', startIndex);
-			wHand = line.substring(startIndex, endIndex);
-			line = line.substring(endIndex + 4);
-		}
-		
-		return wHand;
-	}
-	
-	private String parseWeaponName()
-	{
-		String wName = "";
-		int startIndex, endIndex, searchIndex;
-		String searchTerm = "\"";
-		searchIndex = line.indexOf(searchTerm);
-		
-		if(searchIndex >= 0)
-		{
-			startIndex = searchIndex + 1;
-			endIndex = line.indexOf("\"", startIndex);
-			wName = line.substring(startIndex, endIndex);
-//			line = line.substring(endIndex + 2);
-		}
-				
-		return wName;
-	}
+//	private String parseWeaponID()
+//	{
+//		String wHand = "";
+//		int startIndex, endIndex, searchIndex;
+//		String searchTerm = "[WEAPON";
+//		searchIndex = line.indexOf(searchTerm);
+//		
+//		if(searchIndex >= 0)
+//		{
+//			startIndex = searchIndex + searchTerm.length() + 4;
+//			endIndex = line.indexOf(')', startIndex);
+//			wHand = line.substring(startIndex, endIndex);
+//			line = line.substring(endIndex + 4);
+//		}
+//		
+//		return wHand;
+//	}
+//	
+//	private String parseWeaponName()
+//	{
+//		String wName = "";
+//		int startIndex, endIndex, searchIndex;
+//		String searchTerm = "\"";
+//		searchIndex = line.indexOf(searchTerm);
+//		
+//		if(searchIndex >= 0)
+//		{
+//			startIndex = searchIndex + 1;
+//			endIndex = line.indexOf("\"", startIndex);
+//			wName = line.substring(startIndex, endIndex);
+////			line = line.substring(endIndex + 2);
+//		}
+//				
+//		return wName;
+//	}
 	
 	@Override
 	public String toString() {
 		String str = "";
 		str = str.concat(super.toString());
-		str = str.concat("WeaponID: " + this.weaponID + "\n");
-		str = str.concat("WeaponName: " + this.weaponName + "\n");
+//		str = str.concat("WeaponID: " + this.weaponID + "\n");
+//		str = str.concat("WeaponName: " + this.weaponName + "\n");
 		str = str.concat("------------------\n");
 		
 		return str;
