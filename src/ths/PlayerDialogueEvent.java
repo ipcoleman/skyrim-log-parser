f937c7a7602793091567602135d513f8adadb934
Ian Coleman <colemip@gmail.com>
2013-02-21 22:09:22 -0500

    [#25] Adding regex parsing. Adding 2 new attrs: questName, questID.

--- src/ths/PlayerDialogueEvent.java
+++ src/ths/PlayerDialogueEvent.java
@@ -1,14 +1,24 @@
 package src.ths;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PlayerDialogueEvent extends Event {
 
-	private String actorID;
-	private String actorName;
 	private String topicID;
+	private String actorID;
+	private String questName;
+	private String questID;
+//	private String actorName;
+	
+	public PlayerDialogueEvent()
+	{
+		super("");
+	}
 	
 	public PlayerDialogueEvent(String line) {
 		super(line);
-		// TODO Auto-generated constructor stub
+		parse();
 	}
 	
 	public String getActorID() {
@@ -19,13 +29,13 @@ public class PlayerDialogueEvent extends Event {
 		this.actorID = actorID;
 	}
 
-	public String getActorName() {
-		return actorName;
-	}
-
-	public void setActorName(String actorName) {
-		this.actorName = actorName;
-	}
+//	public String getActorName() {
+//		return actorName;
+//	}
+//
+//	public void setActorName(String actorName) {
+//		this.actorName = actorName;
+//	}
 
 	public String getTopicID() {
 		return topicID;
@@ -35,16 +45,95 @@ public class PlayerDialogueEvent extends Event {
 		this.topicID = topicID;
 	}
 
+	public String getQuestName() {
+		return questName;
+	}
+
+	public void setQuestName(String questName) {
+		this.questName = questName;
+	}
+
+	public String getQuestID() {
+		return questID;
+	}
+
+	public void setQuestID(String questID) {
+		this.questID = questID;
+	}
+
 	@Override
 	protected void parse() {
-		// TODO Auto-generated method stub
+		//PLAYER_DIALOGUE [TIF__01009353 <topic info 02009353 on quest ths1_MissingDog (02009344)>] NPC_SPEAKER [Actor < (02089359)>]
 		super.parse();
+		try {
+			parsePlayerDialogue();
+		} catch (IncorrectTagException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parsePlayerDialogue() throws IncorrectTagException
+	{
+		if(this.tag.equals("PLAYER_DIALOGUE"))
+		{
+			setTopicID(parseTopicID());
+			setActorID(parseActorID());
+		}
+		else
+		{
+			throw new IncorrectTagException();
+		}
+	}
+	
+	private String parseTopicID()
+	{
+		String form = "";		
+		String searchRegex = "(\\[{1})TIF__([a-zA-Z0-9]+)(\\s*)(\\<{1})topic info(\\s*)([a-zA-Z0-9]+)(\\s+)on quest(\\s+)([_a-zA-Z0-9\\s]+)(\\({1})([a-zA-Z0-9]+)(\\){1}\\>{1}\\]{1})"; // e.g. [TIF__01009353 <topic info 02009353 on quest ths1_MissingDog (02009344)>]
+		Pattern pattern = Pattern.compile(searchRegex);
+	    Matcher matcher = pattern.matcher(line);
+	    
+		if(matcher.find())
+		{
+			form = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$6");
+			// also set quest info while we're parsing this line
+			this.questName = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$9");
+			this.questID = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$11");
+			
+			// chop off formID from line
+			line = line.substring(matcher.end());	
+		}
+		
+		return form;
+	}
+	
+	private String parseActorID()
+	{
+		String form = "";		
+		String searchRegex = "(\\[{1})Actor(\\s*)(\\<{1})(\\s*)(\\({1})([a-zA-Z0-9]+)(\\){1}\\>{1}\\]{1})"; // e.g. [Actor < (02089359)>]
+		Pattern pattern = Pattern.compile(searchRegex);
+	    Matcher matcher = pattern.matcher(line);
+	    
+		if(matcher.find())
+		{
+			form = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$6");
+			// chop off formID from line
+			line = line.substring(matcher.end());	
+		}
+		
+		return form;
 	}
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return "";
+		String str = "";
+		str = str.concat(super.toString());
+		str = str.concat("TopicID: " + this.topicID + "\n");		
+		str = str.concat("QuestName: " + this.questName + "\n");
+		str = str.concat("QuestID: " + this.questID + "\n");
+		str = str.concat("ActorID: " + this.actorID + "\n");
+		str = str.concat("------------------\n");
+		
+		return str;
 	}
 
 }
