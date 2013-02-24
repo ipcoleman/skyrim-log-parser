dafee3a42e547891742ee50590dfe6db0e54007f
Ian Coleman <colemip@gmail.com>
2013-02-24 18:53:06 -0500

    Adding serialVersionUID, auto-filled by Eclipse, what for?

--- src/ths/IncorrectTagException.java
+++ src/ths/IncorrectTagException.java
@@ -2,6 +2,11 @@ package src.ths;
 
 public class IncorrectTagException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7944529490572087154L; //?
+
 	public IncorrectTagException() {
 		// TODO Auto-generated constructor stub
 		super("INCORRECT TAG");
