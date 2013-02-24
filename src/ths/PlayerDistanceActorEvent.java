64d87f439ecf3c54c5331c2c0a00bc7b914a4f84
Ian Coleman <colemip@gmail.com>
2013-02-23 20:46:48 -0500

    [#25] Adding regex parsing

--- src/ths/PlayerDistanceActorEvent.java
+++ src/ths/PlayerDistanceActorEvent.java
@@ -1,5 +1,8 @@
 package src.ths;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PlayerDistanceActorEvent extends Event {
 
 	private String actorID;
@@ -8,7 +11,7 @@ public class PlayerDistanceActorEvent extends Event {
 	
 	public PlayerDistanceActorEvent(String line) {
 		super(line);
-		// TODO Auto-generated constructor stub
+		parse();	
 	}
 
 	public String getActorID() {
@@ -37,14 +40,86 @@ public class PlayerDistanceActorEvent extends Event {
 
 	@Override
 	protected void parse() {
-		// TODO Auto-generated method stub
 		super.parse();
+		try {
+			parsePlayerDistanceActor();
+		} catch (IncorrectTagException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parsePlayerDistanceActor() throws IncorrectTagException
+	{
+		if(this.tag.equals("PLAYER_DISTANCE_ACTOR"))
+		{
+			setActorID(parseActorID());
+			setActorName(parseActorName());
+			setDistance(parseDistance());		
+		}
+		else
+		{
+			throw new IncorrectTagException();
+		}
+	}
+	
+	private String parseActorID()
+	{
+		String form = "";		
+		String searchRegex = "(\\[{1})([a-zA-Z]+)(\\s*)(\\<{1})(\\s*)(\\({1})([a-zA-Z0-9]+)(\\){1}\\>{1}\\]{1})"; // e.g. [ObjectReference < (02089359)>]
+		Pattern pattern = Pattern.compile(searchRegex);
+	    Matcher matcher = pattern.matcher(line);
+	    
+		if(matcher.find())
+		{
+			form = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$7");
+			// chop off formID from line
+			line = line.substring(matcher.end());	
+		}
+		
+		return form;
+	}
+	
+	private String parseActorName()
+	{
+		String fName = "";
+		String searchRegex = "(\\\"{1})([a-zA-Z0-9\\s]+)(\\\"{1})"; //e.g. "Form Name"
+		Pattern pattern = Pattern.compile(searchRegex);
+		Matcher matcher = pattern.matcher(line);
+		if(matcher.find())
+		{
+			fName = line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$2");
+			line = line.substring(matcher.end());
+		}
+				
+		return fName;
+	}
+	
+	private double parseDistance()
+	{
+		double fName = 0.0;
+		String searchRegex = "([0-9]+)([.]{1})([0-9]+)"; //e.g. 1160.196533
+		Pattern pattern = Pattern.compile(searchRegex);
+		Matcher matcher = pattern.matcher(line);
+		if(matcher.find())
+		{
+			fName = Double.parseDouble(line.substring(matcher.start(), matcher.end()).replaceFirst(searchRegex, "$1$2$3"));
+			fName = Math.round(fName);
+			line = line.substring(matcher.end());
+		}
+				
+		return fName;
 	}
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return "";
+		String str = "";
+		str = str.concat(super.toString());
+		str = str.concat("ActorID: " + this.actorID + "\n");
+		str = str.concat("ActorName: " + this.actorName + "\n");
+		str = str.concat("Distance: " + this.distance + "\n");
+		str = str.concat("------------------\n");
+		
+		return str;
 	}
 
 }
