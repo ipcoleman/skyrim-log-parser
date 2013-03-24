1a5e78cbdec09b6d6d3e0e57dc4054db4a337692
Ian Coleman <colemip@gmail.com>
2013-03-24 15:16:34 -0400

    Cleaning comments

--- src/ths/Main.java
+++ src/ths/Main.java
@@ -11,93 +11,82 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		String line = null;
-//		String tag = null;
 		String logRoot = "logs/phase2/";
 		File[] files = new File(logRoot).listFiles();
 		Parser parser;
 		input = new Scanner(System.in);
-		
-		/* TESTING */
-//		String pattern = "(?i)(<title.*?>)(.+?)(</title>)";
-//		String example = "<title>you should get this</title>";
-//		String updated = example.replaceAll(pattern, "$2"); 
-//		System.out.print(updated);
-//		System.exit(0);				
-						
+
 		/* iterate over all log files */
-		for (File subjectDir : files) 
-		{
-			if(subjectDir.isDirectory())
-			{
+		for (File subjectDir : files) {
+			if (subjectDir.isDirectory()) {
 				/* iterate over role & self directories for subject id */
-				for (File playTypeDir : subjectDir.listFiles()) 
-				{					
-					if(playTypeDir.isDirectory())
-					{
+				for (File playTypeDir : subjectDir.listFiles()) {
+					if (playTypeDir.isDirectory()) {
 						/* enter 'self' dir */
-						if(playTypeDir.getName().equals("self"))
-						{
+						if (playTypeDir.getName().equals("self")) {
 							/* parse self files */
-							for(File roleFile : playTypeDir.listFiles())
-							{
-								parser = new Parser(logRoot + subjectDir.getName() + "/self/" + roleFile.getName());
-								/* set name of file for parser to output to file w/ same name */
+							for (File roleFile : playTypeDir.listFiles()) {
+								parser = new Parser(logRoot
+										+ subjectDir.getName() + "/self/"
+										+ roleFile.getName());
+								/*
+								 * set name of file for parser to output to file
+								 * w/ same name
+								 */
 								parser.setFileName(roleFile.getName() + "_self");
-								System.out.println("FILE NAME: " + roleFile.getPath());
-								try{
-									parser.parse();
-								} catch(Exception e)
-								{
-									e.printStackTrace();
-									System.exit(0);
-								}								
-								System.out.println("FINISHED FILE: " + roleFile.getPath());
-								
-//								parser.printIntervalOfPlayerMoveEvents();
-							}
-						}
-												
-						/* enter 'role' dir */
-						if(playTypeDir.getName().equals("role"))
-						{
-							/* parse role files */
-							for(File roleFile : playTypeDir.listFiles())
-							{
-								parser = new Parser(logRoot + subjectDir.getName() + "/role/" + roleFile.getName());
-								/* set name of file for parser to output to file w/ same name */
-								parser.setFileName(roleFile.getName() + "_role");
-								System.out.println("FILE NAME: " + roleFile.getPath());
+								System.out.println("FILE NAME: "
+										+ roleFile.getPath());
 								try {
-									parser.parse();	
-								} catch(Exception e)
-								{
+									parser.parse();
+								} catch (Exception e) {
 									e.printStackTrace();
 									System.exit(0);
 								}
-//								parser.printIntervalOfPlayerMoveEvents();
-								System.out.println("FINISHED FILE: " + roleFile.getPath());
-								
-//								parser.printIntervalOfPlayerMoveEvents();
+								System.out.println("FINISHED FILE: "
+										+ roleFile.getPath());
+
+								// parser.printIntervalOfPlayerMoveEvents();
 							}
 						}
-						
-						
-						
+
+						/* enter 'role' dir */
+						if (playTypeDir.getName().equals("role")) {
+							/* parse role files */
+							for (File roleFile : playTypeDir.listFiles()) {
+								parser = new Parser(logRoot
+										+ subjectDir.getName() + "/role/"
+										+ roleFile.getName());
+								/*
+								 * set name of file for parser to output to file
+								 * w/ same name
+								 */
+								parser.setFileName(roleFile.getName() + "_role");
+								System.out.println("FILE NAME: "
+										+ roleFile.getPath());
+								try {
+									parser.parse();
+								} catch (Exception e) {
+									e.printStackTrace();
+									System.exit(0);
+								}
+								// parser.printIntervalOfPlayerMoveEvents();
+								System.out.println("FINISHED FILE: "
+										+ roleFile.getPath());
+
+								// parser.printIntervalOfPlayerMoveEvents();
+							}
+						}
+
 						/* ask if we should continue */
-//						System.out.print("Continue (y/n)? > ");
-//						String choice = input.nextLine();
-//						// exit program
-//						if(choice.equals("n"))
-//							System.exit(0);
+						// System.out.print("Continue (y/n)? > ");
+						// String choice = input.nextLine();
+						// // exit program
+						// if(choice.equals("n"))
+						// System.exit(0);
 					}
 				}
-					
-					
+
 			}
 		}
-		
-							
 	}
 }
--- src/ths/Parser.java
+++ src/ths/Parser.java
@@ -10,205 +10,177 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Dictionary;
 import java.util.ListIterator;
 import org.joda.time.Interval;
 
 public class Parser {
 
-	private String				fileName;
-	private FileInputStream 	fInputStream;
-	private DataInputStream 	dInputStream;
-	private BufferedReader 		buffReader;
-	private String[] 			logTags;
-	private int					tagCount;
-	private ArrayList<Event>	events;
-	private EventFactory		factory;
-	private static int			currentLine;
-	private PrintWriter			out;
-	private PrintWriter			csvOut;
-	
-	
+	private String fileName;
+	private FileInputStream fInputStream;
+	private DataInputStream dInputStream;
+	private BufferedReader buffReader;
+	private String[] logTags;
+	private int tagCount;
+	private ArrayList<Event> events;
+	private EventFactory factory;
+	private static int currentLine;
+	private PrintWriter out;
+	private PrintWriter csvOut;
+
 	public Parser() {
 	}
-	
-	public Parser(String file)
-	{
+
+	public Parser(String file) {
 		fileName = file;
-		try {			
+		try {
 			fInputStream = new FileInputStream(file);
 			dInputStream = new DataInputStream(fInputStream);
 			buffReader = new BufferedReader(new InputStreamReader(dInputStream));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		logTags = new String[100];
 		tagCount = 0;
 		events = new ArrayList<Event>();
 		factory = new EventFactory();
 		currentLine = 1;
 	}
-	
+
 	public String getFileName() {
 		return fileName;
 	}
-	
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	
-	public void parse()
-	{
+
+	public void parse() {
 		String line;
 		Event e;
 
 		try {
-			out = new PrintWriter(new FileWriter("logs/output/" + this.fileName + ".txt"));
+			out = new PrintWriter(new FileWriter("logs/output/" + this.fileName
+					+ ".txt"));
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		while((line = nextLine()) != null)
-		{
+
+		while ((line = nextLine()) != null) {
 			System.out.println("Line " + currentLine + ": " + line + "\n");
 			e = factory.makeEvent(line);
-//			System.out.println(e);
+			// System.out.println(e);
 			out.print(e.toString());
-			
+
 			events.add(e);
 			currentLine++;
 		}
-		
+
 		printIntervalOfPlayerMoveEvents();
-		
+
 		out.close();
-		if(csvOut != null)
+		if (csvOut != null)
 			csvOut.close();
 	}
-	
-	public String nextLine()
-	{
+
+	public String nextLine() {
 		String line = null;
-		
+
 		try {
 			line = buffReader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return line;
 	}
-	
-	public boolean containsTag(String tag)
-	{	
+
+	public boolean containsTag(String tag) {
 		try {
-			for(int i=0; i<logTags.length; i++)
-			{
-				if(tag.equals(logTags[i]))
-				{
+			for (int i = 0; i < logTags.length; i++) {
+				if (tag.equals(logTags[i])) {
 					return true;
 				}
 			}
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
-	
-	public void addTag(String tag)
-	{
-		try
-		{
+
+	public void addTag(String tag) {
+		try {
 			logTags[tagCount++] = tag;
-		}
-		catch(ArrayIndexOutOfBoundsException e)
-		{
+		} catch (ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public boolean addEvent(Event e)
-	{
+
+	public boolean addEvent(Event e) {
 		return this.events.add(e);
 	}
-	
-	public Interval calcPlayerMoveInterval(Event e)
-	{
+
+	public Interval calcPlayerMoveInterval(Event e) {
 		Interval i = null;
 		Event nextEvent;
 		Date nextTimestamp = null;
 		int index;
 		ListIterator<Event> li;
-		
+
 		index = events.indexOf(e);
-		if(index >= 0) // object found
+		if (index >= 0) // object found
 		{
-			li = events.listIterator(index+1); // iterator over events after e
-			while(li.hasNext())
-			{
+			li = events.listIterator(index + 1); // iterator over events after e
+			while (li.hasNext()) {
 				nextEvent = li.next();
-				if(nextEvent instanceof PlayerMoveEvent)
-				{
+				if (nextEvent instanceof PlayerMoveEvent) {
 					nextTimestamp = nextEvent.getTimestamp();
 					break;
 				}
 			}
-			if(nextTimestamp != null)
-				i = new Interval(e.getTimestamp().getTime(), nextTimestamp.getTime());
+			if (nextTimestamp != null)
+				i = new Interval(e.getTimestamp().getTime(),
+						nextTimestamp.getTime());
 		}
-		
+
 		return i;
 	}
-	
-	
-	public void printIntervalOfPlayerMoveEvents()
-	{
+
+	public void printIntervalOfPlayerMoveEvents() {
 		Event e;
 		Interval i;
-		
+
 		ListIterator<Event> li = events.listIterator();
-		while(li.hasNext())
-		{
+		while (li.hasNext()) {
 			e = li.next();
-			if(e instanceof PlayerMoveEvent)
-			{
+			if (e instanceof PlayerMoveEvent) {
 				i = calcPlayerMoveInterval(e);
-				if(i != null)
-				{
+				if (i != null) {
 					System.out.println("PlayerMoveEvent Interval: " + i);
 					out.println("PlayerMoveEvent Interval: " + i);
-					outputIntervalOfPlayerMoveEvent((PlayerMoveEvent)e, i);
+					outputIntervalOfPlayerMoveEvent((PlayerMoveEvent) e, i);
 				}
 			}
 		}
 	}
-	
-	private void outputIntervalOfPlayerMoveEvent(PlayerMoveEvent e, Interval i)
-	{
+
+	private void outputIntervalOfPlayerMoveEvent(PlayerMoveEvent e, Interval i) {
 		try {
-//			if(e == null)
-//				System.out.print("Event null");
-//			if(i == null)
-//				System.out.print("Interval null");
-			System.out.println(e.getType() + "," + i.toDurationMillis()/1000);
-		}
-		catch(Exception ex)
-		{
+			System.out.println(e.getType() + "," + i.toDurationMillis() / 1000);
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 		/* print to file */
 		try {
-			csvOut = new PrintWriter(new FileWriter("logs/output/" + this.fileName + ".csv"));
-			csvOut.println(e.getType() + "," + i.toDurationMillis()/1000);
+			csvOut = new PrintWriter(new FileWriter("logs/output/"
+					+ this.fileName + ".csv"));
+			csvOut.println(e.getType() + "," + i.toDurationMillis() / 1000);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 	}
-	
+
 }
