58fcf5277dd3bfe3c27f5d6a8e1c953c0523e5bb
Ian Coleman <colemip@gmail.com>
2013-04-09 22:24:55 -0400

    [#46] Adding hash map for non-reversed score items

--- src/ths/BFIScorer.java
+++ src/ths/BFIScorer.java
@@ -13,7 +13,9 @@ public class BFIScorer {
 	private Connection connection;
 	private Statement statement;
 	private ResultSet results;
-	private HashMap<String, int[]> reverseScoreItems; 
+	private HashMap<String, int[]> regularScoreItems;
+	private HashMap<String, int[]> reverseScoreItems;
+	private String[] reverseScoreArray;
 	
 	public BFIScorer()
 	{
@@ -22,12 +24,20 @@ public class BFIScorer {
 	
 	private void initReverseScoreItems()
 	{
+		/* Score HashMap's */
 		reverseScoreItems = new HashMap<String, int[]>();
 		reverseScoreItems.put("extraversion", new int[]{ 6, 21, 31 });
 		reverseScoreItems.put("agreeableness", new int[]{ 2, 12, 27, 37 });
 		reverseScoreItems.put("conscientiousness", new int[]{ 8, 18, 23, 43 });
 		reverseScoreItems.put("neuroticism", new int[]{ 9, 24, 34 });
 		reverseScoreItems.put("openness", new int[]{ 35, 41 });
+		
+		regularScoreItems = new HashMap<String, int[]>();
+		regularScoreItems.put("extraversion", new int[]{ 1, 11, 16, 26, 36 });
+		regularScoreItems.put("agreeableness", new int[]{ 7,  17, 22,  32,  42 });
+		regularScoreItems.put("conscientiousness", new int[]{ 3, 13, 28, 33, 38 });
+		regularScoreItems.put("neuroticism", new int[]{ 4, 14, 19, 29, 39 });
+		regularScoreItems.put("openness", new int[]{ 5, 10, 15, 20, 25, 30, 40, 44 });
 	}
 	
 	public HashMap<String, int[]> getReverseScoreItems()
@@ -64,14 +74,16 @@ public class BFIScorer {
 			
 			while(this.results.next())
 			{
-				System.out.println(this.results.getString(1));
+				System.out.println(this.results.getString(2) + ", " + this.results.getString(3) + ", " + this.results.getString(4));
 				BFIScorer bfi = new BFIScorer();
 				HashMap<String, int[]> reverseScoreItems = bfi.getReverseScoreItems();
 //				String traits[] = {"openness", "conscientiousness", "extraversion", "agreeableness", "neuroticism"};
 				Set<String> keySet = reverseScoreItems.keySet();
 				for(int i= 0; i<keySet.size(); i++)
 				{
-					int nextValueArray[] = reverseScoreItems.get(keySet.iterator().next());   
+					String key = keySet.iterator().next();
+					System.out.println("Key: " + key);
+					int nextValueArray[] = reverseScoreItems.get(key);   
 					for(int j = 0; j < nextValueArray.length; j++)
 					{
 						System.out.println(nextValueArray[j]);
