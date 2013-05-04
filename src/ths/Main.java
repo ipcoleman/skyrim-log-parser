13ad689a681b1546688943e080f8d4620bc0950e
Ian Coleman <colemip@gmail.com>
2013-05-04 18:52:39 -0400

    Combining role and self parsers, forking based on directory

--- src/ths/Main.java
+++ src/ths/Main.java
@@ -6,21 +6,19 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-
-	private static Scanner input;
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String logRoot = "logs/phase2/";
+		String logPath = "";
 		File[] files = new File(logRoot).listFiles();
 		Parser parser;
-		input = new Scanner(System.in);
-		BFIScorer bfiRole = new BFIScorer();
-		bfiRole.connectToDatabase();
-		BFIScorer bfiSelf = new BFIScorer();
-		bfiSelf.connectToDatabase();
+		BFIScorer bfi = new BFIScorer();
+		bfi.connectToDatabase();
+//		BFIScorer bfiSelf = new BFIScorer();
+//		bfiSelf.connectToDatabase();
 		
 		/* iterate over all log files */
 		for (File subjectDir : files) {
@@ -28,86 +26,94 @@ public class Main {
 				/* iterate over role & self directories for subject id */
 				for (File playTypeDir : subjectDir.listFiles()) {
 					if (playTypeDir.isDirectory()) {
-						/* enter 'self' dir */
-						if (playTypeDir.getName().equals("self")) {
-							/* parse self files */
-							for (File roleFile : playTypeDir.listFiles()) {
-								parser = new Parser(logRoot
+						
+						for (File logFile : playTypeDir.listFiles()) {
+							/* set proper log file directory (self vs role) */
+							if (playTypeDir.getName().equals("self")) {
+								logPath = logRoot
 										+ subjectDir.getName() + "/self/"
-										+ roleFile.getName());
+										+ logFile.getName();
+							}
+							else if (playTypeDir.getName().equals("role")) {
+								logPath = logRoot
+										+ subjectDir.getName() + "/role/"
+										+ logFile.getName();
+							}						
+														
+							
+								parser = new Parser(logPath);
 								/*
 								 * set name of file for parser to output to file
 								 * w/ same name
 								 */
-								parser.setFileName(roleFile.getName() + "_self");
+								parser.setFileName(logFile.getName() + "_self");
 								System.out.println("FILE NAME: "
-										+ roleFile.getPath());
+										+ logFile.getPath());
 								try {
 //									parser.parse();
 								} catch (Exception e) {
 									e.printStackTrace();
 									System.exit(0);
 								}
-								
-								/* print BFI score */
-//								BFIScorer bfiRole = new BFIScorer(roleFile.getName() + "_role");
+
 								try {
-									bfiRole.setFileName(roleFile.getName() + "_role");
+									bfi.setFileName(logFile.getName() + "_role");
 								} catch (IOException e) {
-									// TODO Auto-generated catch block
 									e.printStackTrace();
 								}
 								int subjectID = Integer.parseInt(subjectDir.getName());
-								bfiRole.getPersonalityIndexResults(subjectID);
-								bfiRole.printBFIScoresToCsv(subjectID);
+								bfi.getPersonalityIndexResults(subjectID);
+								bfi.printBFIScoresToCsv(subjectID);
 								
 								System.out.println("FINISHED FILE: "
-										+ roleFile.getPath());
+										+ logFile.getPath());
 
 								 parser.printIntervalOfPlayerMoveEvents();
 							}
 						}
 
+						
+						
 						/* enter 'role' dir */
-						if (playTypeDir.getName().equals("role")) {
-							/* parse role files */
-							for (File roleFile : playTypeDir.listFiles()) {
-								parser = new Parser(logRoot
-										+ subjectDir.getName() + "/role/"
-										+ roleFile.getName());
-								/*
-								 * set name of file for parser to output to file
-								 * w/ same name
-								 */
-								parser.setFileName(roleFile.getName() + "_role");
-								System.out.println("FILE NAME: "
-										+ roleFile.getPath());
-								try {
-//									parser.parse();
-								} catch (Exception e) {
-									e.printStackTrace();
-									System.exit(0);
-								}
-								
-								/* print BFI score */
-//								BFIScorer bfiSelf = new BFIScorer(roleFile.getName() + "_self");
-								try {
-									bfiSelf.setFileName(roleFile.getName() + "_self");
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-								int subjectID = Integer.parseInt(subjectDir.getName());
-								bfiSelf.getPersonalityIndexResults(subjectID);
-								bfiSelf.printBFIScoresToCsv(subjectID);
-								
-								// parser.printIntervalOfPlayerMoveEvents();
-								System.out.println("FINISHED FILE: "
-										+ roleFile.getPath());
-
-								// parser.printIntervalOfPlayerMoveEvents();
-							}
-						}
-					}
+//						if (playTypeDir.getName().equals("role")) {
+//							/* parse role files */
+//							for (File roleFile : playTypeDir.listFiles()) {
+//								parser = new Parser(logRoot
+//										+ subjectDir.getName() + "/role/"
+//										+ roleFile.getName());
+//								/*
+//								 * set name of file for parser to output to file
+//								 * w/ same name
+//								 */
+//								parser.setFileName(roleFile.getName() + "_role");
+//								System.out.println("FILE NAME: "
+//										+ roleFile.getPath());
+//								try {
+////									parser.parse();
+//								} catch (Exception e) {
+//									e.printStackTrace();
+//									System.exit(0);
+//								}
+//								
+//								/* print BFI score */
+////								BFIScorer bfiSelf = new BFIScorer(roleFile.getName() + "_self");
+//								try {
+//									bfiSelf.setFileName(roleFile.getName() + "_self");
+//								} catch (IOException e) {
+//									e.printStackTrace();
+//								}
+//								int subjectID = Integer.parseInt(subjectDir.getName());
+//								bfiSelf.getPersonalityIndexResults(subjectID);
+//								bfiSelf.printBFIScoresToCsv(subjectID);
+//								
+//								// parser.printIntervalOfPlayerMoveEvents();
+//								System.out.println("FINISHED FILE: "
+//										+ roleFile.getPath());
+//
+//								// parser.printIntervalOfPlayerMoveEvents();
+//							}
+//						}
+//					}
 				}
 			}							
 		}
